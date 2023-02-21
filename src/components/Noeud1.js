f50dc1d0ff3d95c626736ec6bbff22b66cc8625a
Naoufali MADI <naoufali@hotmail.fr>
2023-02-21 09:46:25 +0100

    TP02

---
+++ src/components/Noeud1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const noeud1 = () => {
+    return (
+        <div>
+             <h1>Naoufali MADI</h1>
+        </div>
+    );
+};
+
+export default noeud1;
---
+++ src/components/Noeud2.js
@@ -0,0 +1,18 @@
+/**Noeud avec une classe  */
+
+import React, { Component } from 'react';
+
+class Noeud2 extends Component {
+    render() {
+        return (
+            <div>
+               <h2>Contenu dans l'application ReactJs</h2> 
+            </div>
+        );
+    }
+}
+
+export default Noeud2;
+
+
+
